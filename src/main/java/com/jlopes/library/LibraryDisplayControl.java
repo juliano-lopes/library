f3efe9e5f24bd405743e73ec616e975777f19d69
juliano-lopes <julopys@hotmail.com>
2016-09-21 16:10:25 -0300

    Created method 'displayWithListBooks', 'displayWithListBooksToReturn'.

--- src/main/java/com/jlopes/library/LibraryDisplayControl.java
+++ src/main/java/com/jlopes/library/LibraryDisplayControl.java
@@ -3,6 +3,7 @@ package com.jlopes.library;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.Scanner;
 
 public class LibraryDisplayControl {
@@ -16,15 +17,12 @@ public class LibraryDisplayControl {
 
 	public int menuOptions() {
 
-		System.out
-				.println(""
-						+ "O que gostaria de fazer?\n\n"
-						+ "1. - Buscar por um livro;\n"
-						+ "2. - Listar todos os livros;\n"
-						+ "3. - Listar somente os livros disponíveis para retirada;\n"
-						+ "4. - Listar os livros não disponíveis para conhecer o que as pessoas andam lendo no momento;\n"
-						+ "5. - Realizar a devolução de um livro;\n"
-						+ "6. - Sair do sistema...\n");
+		System.out.println("" + "O que gostaria de fazer?\n\n"
+				+ "1. - Buscar por um livro;\n"
+				+ "2. - Listar todos os livros;\n"
+				+ "3. - Listar somente os livros disponíveis para retirada;\n"
+				+ "4. - Realizar a devolução de um livro;\n"
+				+ "5. - Sair do sistema...\n");
 		entry = new Scanner(System.in);
 
 		return entry.nextInt();
@@ -42,14 +40,14 @@ public class LibraryDisplayControl {
 		return search.toLowerCase();
 	}
 
-	public int displayWithAllTheBooks() {
+	public int displayWithListBooks(List<Books> listBooks) {
 
 		System.out
-				.println("Veja o que temos em nossa biblioteca para você!\n"
-						+ "Caso queira realizar a retirada de alguma das obras exibidas, por favor digite o número correspondente a ela.\n"
+				.println("Veja a lista de livros escolhida. \n"
+						+ "Caso queira realizar a retirada de alguma das obras exibidas, por favor digite o número correspondente à ela, quando houver. \n"
 						+ "Para voltar ao menu digite '0': \n\n");
 		int arrow = 1;
-		for (Books book : this.library.getBooks()) {
+		for (Books book : listBooks) {
 			System.out.println("LIVRO " + arrow + "\n" + "Título: "
 					+ book.getTitle() + "\n" + "Autor: " + book.getAuthor()
 					+ "\n" + "Gênero: " + book.getGenrer() + "\n"
@@ -63,4 +61,26 @@ public class LibraryDisplayControl {
 		return entry.nextInt();
 	}
 
+	public int displayWithListBooksToReturn(List<CheckBooksOut> listBooks) {
+		System.out
+				.println("Veja aqui o (s) livro (s) que identificamos em seu nome.\n"
+						+ "Caso queira realizar a devolução, digite o número que corresponde ao livro.\n"
+						+ "Para voltar ao menu digite '0':\n\n");
+		int arrow = 1;
+		for (CheckBooksOut book : listBooks) {
+			System.out.println("LIVRO " + arrow + "\n" + "Título: "
+					+ book.getBook().getTitle() + "\n" + "Autor: "
+					+ book.getBook().getAuthor() + "\n" + "Gênero: "
+					+ book.getBook().getGenrer() + "\n"
+					+ "Tipo de Literatura: "
+					+ book.getBook().getKindOfLiterature() + "\n" + "Editora: "
+					+ book.getBook().getPublisher() + "\n"
+					+ "====================\n\n");
+			arrow++;
+		}
+		entry = new Scanner(System.in);
+
+		return entry.nextInt();
+
+	}
 }
--- src/test/java/com/jlopes/library/LibraryDisplayControlTest.java
+++ src/test/java/com/jlopes/library/LibraryDisplayControlTest.java
@@ -33,8 +33,43 @@ public class LibraryDisplayControlTest {
 		LibraryManager library = new LibraryManager(libraryService.getBooks());
 		LibraryDisplayControl displayControl = new LibraryDisplayControl(
 				library);
-		int option = displayControl.displayWithAllTheBooks();
+		int option = displayControl.displayWithListBooks(library.getBooks());
 		assertThat(option, is(3));
+	}
+
+	@Test
+	public void shouldReturnOptionChoosenInTheListWithAvailableBooks() {
+		LibraryService libraryService = new LibraryService();
+		LibraryManager library = new LibraryManager(libraryService.getBooks());
+		LibraryDisplayControl displayControl = new LibraryDisplayControl(
+				library);
+		Users user = new Users("Juliano", "julopys@hotmail.com", "31993709668");
+		Books book = library.searchedBook("Se Houver Amanhã");
+		CheckBooksOut myBook = new CheckBooksOut(user, book);
+		library.checkingBookOut(myBook);
+		int option = displayControl.displayWithListBooks(library
+				.availableBooks());
+		assertThat(option, is(2));
 
 	}
+
+	@Test
+	public void shouldReturnOptionChoosenInTheListToReturn() {
+		LibraryService libraryService = new LibraryService();
+		LibraryManager library = new LibraryManager(libraryService.getBooks());
+		LibraryDisplayControl displayControl = new LibraryDisplayControl(
+				library);
+		Users user = new Users("Juliano", "julopys@hotmail.com", "31993709668");
+		Books book = library.searchedBook("Se Houver Amanhã");
+		CheckBooksOut myBook = new CheckBooksOut(user, book);
+		library.checkingBookOut(myBook);
+		book = library.searchedBook("As Aventuras de Sharpe2");
+		myBook = new CheckBooksOut(user, book);
+		library.checkingBookOut(myBook);
+		int option = displayControl.displayWithListBooksToReturn(library
+				.checkedBooksOutByUser(user.getName()));
+		assertThat(option, is(2));
+
+	}
+
 }
