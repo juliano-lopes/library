a2aca692bad4605b9503442931de8271d3770b65
juliano-lopes <julopys@hotmail.com>
2016-12-01 01:37:12 -0200

    Adding SpringBoot in the project.

---
+++ src/main/java/com/jlopes/library/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.jlopes.library.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@RequestMapping(value = "/home")
+	public String home() {
+		String home = "<doctype html>" + "<html lang='pt-br'>" + "<head>"
+				+ "<title> Biblioteca </title>" + "<meta charset='iso-8859-1'>"
+				+ "</head>" + "<body>" + "<header>" + "<h1>Biblioteca!</h1>"
+				+ "<nav role='menu'>" + "<ul>" + "<li>Listar livros</li>"
+				+ "<li>Listar livros disponíveis</li>"
+				+ "<li>Listar livros retirados</li>"
+				+ "<li>Sair do sistema</li>" + "</ul>" + "</nav>" + "</header>"
+				+ "<div role='main'>" + "</div>" + "</body>" + "</html>";
+		return home;
+	}
+}
