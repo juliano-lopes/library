3713ebf4cc3b4278b74ce723fa7fbc283aab9c97
juliano-lopes <julopys@hotmail.com>
2016-12-01 01:43:30 -0200

    Created method to get all books.

---
+++ src/main/java/com/jlopes/library/controller/BookController.java
@@ -0,0 +1,26 @@
+package com.jlopes.library.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jlopes.library.LibraryManager;
+import com.jlopes.library.domain.Book;
+import com.jlopes.library.service.BookService;
+
+@RestController
+public class BookController {
+	private final LibraryManager library;
+
+	public BookController() {
+		library = new LibraryManager(new BookService());
+	}
+
+	@RequestMapping(value = "/book/all", method = RequestMethod.GET)
+	public List<Book> getAll() {
+		return library.getBooks();
+	}
+
+}
